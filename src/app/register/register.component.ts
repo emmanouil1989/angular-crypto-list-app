b3ea839bdc4e2f8d8cd20d8037b8036111e16744
emmanouil emmanouil <emmanouil.e25@gmail.com>
2024-05-20 12:04:29 +0100

    1) add a register form structure 2)display user name instead of email when user is logged in

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  formbuilder = inject(FormBuilder);
+  http = inject(HttpClient);
+  toaster = inject(ToastrService);
+  router = inject(Router);
+  formSubmitted: boolean = false;
+
+  registerForm = this.formbuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    username: ['', Validators.required],
+  });
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+
+    if (this.registerForm.valid) {
+      console.log('register api call');
+    }
+  }
+}
